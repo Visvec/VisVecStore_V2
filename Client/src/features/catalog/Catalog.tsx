0fc9a0ea52faef25e528c35590fe0aa6ea2bd516
Visvec <gowusu@umat.edu.gh>
2025-04-18 23:43:24 +0000

    End of Section 4

--- Client/src/features/catalog/Catalog.tsx
+++ Client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
@@ -5,7 +6,16 @@ type Props = {
   products: Product [];
 }
 
-export default function Catalog({products}: Props) {
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+  fetch('https://localhost:5001/api/products')
+  .then (response => response.json())
+  .then(data => setProducts(data));
+  }, [])
+
+  
   return (
     <>
      <ProductList products = {products}/>
