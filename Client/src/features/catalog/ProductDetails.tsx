90d3079dcc459ae6bfd12e01f2856254be3fc934
Visvec <gowusu@umat.edu.gh>
2025-04-20 07:58:39 +0000

    End of Section 5

--- Client/src/features/catalog/ProductDetails.tsx
+++ Client/src/features/catalog/ProductDetails.tsx
@@ -1,20 +1,12 @@
 import { useParams } from "react-router-dom"
-import { Product } from "../../app/models/product";
-import { useEffect, useState } from "react";
 import { Button, Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
+import { useFetchProductDetailsQuery } from "./catalogApi";
 
 export default function ProductDetails() {
   const {id} = useParams();
-  const [product, setProduct] = useState<Product |null >(null);
 
-  useEffect(() => {
-
-  fetch(`https://localhost:5001/api/products/${id}`)
-  .then(response => response.json())
-  .then(data => setProduct(data))
-  .catch(error => console.log(error))
-  }, [id])
-  if(!product) return <div>Loading...</div>
+  const {data: product, isLoading} = useFetchProductDetailsQuery(id ? +id : 0)
+  if(!product || isLoading)  return <div>Loading...</div>
 
   const ProductDetails = [
     {label: 'Name', value: product.name },
