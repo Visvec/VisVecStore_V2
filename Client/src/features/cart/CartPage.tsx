dfb9abe29e6ec1a4b11e73aa6f90bff73f764132
Visvec <gowusu@umat.edu.gh>
2025-04-23 17:02:42 +0000

    Modification to the CartPage, whenever the cart is empty. (Added continue shopping link)

--- Client/src/features/cart/CartPage.tsx
+++ Client/src/features/cart/CartPage.tsx
@@ -2,13 +2,23 @@ import { Grid, Typography } from "@mui/material";
 import { useFetchCartQuery } from "./cartApi"
 import CartItem from "./CartItem";
 import OrderSummary from "../../app/shared/components/OrderSummary";
+import { Link } from "react-router-dom";
 
 export default function CartPage() {
   const { data, isLoading } = useFetchCartQuery();
 
   if (isLoading) return <Typography> Loading cart...</Typography>
 
-  if (!data || data.items.length === 0) return <Typography variant="h3"> Your cart is empty</Typography>
+  if (!data || data.items.length === 0) {
+    return (
+    <Typography variant="h3">
+       Your cart is empty.{' '}
+       <Link to = '/catalog' style = {{ fontStyle: 'italic', fontSize: '0.8em', color: 'blue'}} >
+          Continue shopping
+       </Link>
+       </Typography>
+  );
+}
 
   return (
     <Grid container spacing={2}>
