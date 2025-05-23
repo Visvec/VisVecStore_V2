247f2bcae951aad5e845ff4ded8396b9452bf362
Visvec <gowusu@umat.edu.gh>
2025-05-23 07:22:56 +0000

    Adding of Confirmation Password

--- Client/src/features/account/RegisterForm.tsx
+++ Client/src/features/account/RegisterForm.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useRegisterMutation } from "./accountApi"
+import { useRegisterMutation } from "./accountApi";
 import { registerSchema, RegisterSchema } from "../../lib/schemas/registerSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LockOutlined } from "@mui/icons-material";
@@ -7,101 +7,113 @@ import { Container, Paper, Box, Typography, TextField, Button } from "@mui/mater
 import { Link } from "react-router-dom";
 
 export default function RegisterForm() {
-    const [registerUser] = useRegisterMutation();
-    const { register, handleSubmit, setError, formState: { errors, isValid, isLoading } } = useForm<RegisterSchema>({
-        mode: 'onTouched',
-        resolver: zodResolver(registerSchema)
-    })
+  const [registerUser] = useRegisterMutation();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid, isLoading },
+  } = useForm<RegisterSchema>({
+    mode: "onTouched",
+    resolver: zodResolver(registerSchema),
+  });
 
-    const onSubmit = async (data: RegisterSchema) => {
-        try {
-            await registerUser(data).unwrap();
-        } catch (error) {
-            const apiError = error as { message: string };
-            if (apiError.message && typeof apiError.message === 'string') {
-                const errorArray = apiError.message.split(',');
+  const onSubmit = async (data: RegisterSchema) => {
+    try {
+      await registerUser(data).unwrap();
+    } catch (error) {
+      const apiError = error as { message: string };
+      if (apiError.message && typeof apiError.message === "string") {
+        const errorArray = apiError.message.split(",");
 
-                errorArray.forEach(e => {
-                    if (e.includes('Password')) {
-                        setError('password', { message: e })
-                    } else if (e.includes('Email')) {
-                        setError('email', { message: e })
-                    }
-                })
-            }
-
-        }
-
+        errorArray.forEach((e) => {
+          if (e.includes("Password")) {
+            setError("password", { message: e });
+          } else if (e.includes("Email")) {
+            setError("email", { message: e });
+          }
+        });
+      }
     }
-    return (
-        <Container component={Paper} maxWidth='sm' sx={{ borderRadius: 3 }}>
-            <Box display='flex' flexDirection='column' alignItems='center' marginTop='8'>
-                <LockOutlined sx={{ mt: 3, color: 'secondary.main', fontSize: 40 }} />
-                <Typography variant="h5">
-                    Register
-                </Typography>
-                <Box
-                    component='form'
-                    onSubmit={handleSubmit(onSubmit)}
-                    width='100%'
-                    display='flex'
-                    flexDirection='column'
-                    gap={3}
-                    marginY={3}
-                >
-                    <TextField
-                        fullWidth
-                        label='Email'
-                        autoFocus
-                        {...register('email')}
-                        error={!!errors.email}
-                        helperText={errors.email?.message}
-                    />
-                    <TextField
-                        fullWidth
-                        label="First Name"
-                        {...register('firstName')}
-                        error={!!errors.firstName}
-                        helperText={errors.firstName?.message}
-                    />
+  };
 
-                    <TextField
-                        fullWidth
-                        label="Last Name"
-                        {...register('lastName')}
-                        error={!!errors.lastName}
-                        helperText={errors.lastName?.message}
-                    />
+  return (
+    <Container component={Paper} maxWidth="sm" sx={{ borderRadius: 3 }}>
+      <Box display="flex" flexDirection="column" alignItems="center" marginTop="8">
+        <LockOutlined sx={{ mt: 3, color: "secondary.main", fontSize: 40 }} />
+        <Typography variant="h5">Register</Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          width="100%"
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          marginY={3}
+        >
+          <TextField
+            fullWidth
+            label="Email"
+            autoFocus
+            {...register("email")}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            fullWidth
+            label="First Name"
+            {...register("firstName")}
+            error={!!errors.firstName}
+            helperText={errors.firstName?.message}
+          />
 
-                <TextField
-  fullWidth
-  label="Date of Birth"
-  type="date"
-  InputLabelProps={{ shrink: true }}
-  {...register('dateOfBirth')}
-  error={!!errors.dateOfBirth}
-  helperText={errors.dateOfBirth?.message}
-/>
+          <TextField
+            fullWidth
+            label="Last Name"
+            {...register("lastName")}
+            error={!!errors.lastName}
+            helperText={errors.lastName?.message}
+          />
 
-                    <TextField
-                        fullWidth
-                        label='Password'
-                        type="password"
-                        {...register('password')}
-                        error={!!errors.password}
-                        helperText={errors.password?.message}
-                    />
-                    <Button disabled={isLoading || !isValid} variant="contained" type="submit">
-                        Register
-                    </Button>
-                    <Typography sx={{ textAlign: 'center' }}>
-                        Already have an account?
-                        <Typography sx={{ ml: 2 }} component={Link} to='/login' color="primary">
-                            Sign in here
-                        </Typography>
-                    </Typography>
-                </Box>
-            </Box>
-        </Container>
-    )
+          <TextField
+            fullWidth
+            label="Date of Birth"
+            type="date"
+            InputLabelProps={{ shrink: true }}
+            {...register("dateOfBirth")}
+            error={!!errors.dateOfBirth}
+            helperText={errors.dateOfBirth?.message}
+          />
+
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            {...register("password")}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
+
+          <TextField
+            fullWidth
+            label="Confirm Password"
+            type="password"
+            {...register("confirmPassword")}
+            error={!!errors.confirmPassword}
+            helperText={errors.confirmPassword?.message}
+          />
+
+          <Button disabled={isLoading || !isValid} variant="contained" type="submit">
+            Register
+          </Button>
+          <Typography sx={{ textAlign: "center" }}>
+            Already have an account?
+            <Typography sx={{ ml: 2 }} component={Link} to="/login" color="primary">
+              Sign in here
+            </Typography>
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
--- Client/src/lib/schemas/registerSchema.ts
+++ Client/src/lib/schemas/registerSchema.ts
@@ -1,19 +1,28 @@
 import { z } from "zod";
 
-const passwordValidation  = new RegExp(
-    /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/
-)
+const passwordValidation = new RegExp(
+  /(?=^.{6,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{":;'?/&>.<> ,])(?!.*\s).*$/ 
+);
 
-export const registerSchema = z.object({
+export const registerSchema = z
+  .object({
     email: z.string().email(),
     password: z.string().regex(passwordValidation, {
-        message: 'Password must contain 1 lowercase character, 1 uppercase character, 1 number, 1 special and be 6-10 characters'
+      message:
+        "Password must contain 1 lowercase character, 1 uppercase character, 1 number, 1 special and be at least 6 characters long",
     }),
+    confirmPassword: z.string(),
     firstName: z.string().min(1, "First name is required"),
-  lastName: z.string().min(1, "Last name is required"),
-  dateOfBirth: z.string().refine(val => !isNaN(Date.parse(val)), {
-  message: 'Date of birth is required',
-}),
-});
+    lastName: z.string().min(1, "Last name is required"),
+    dateOfBirth: z
+      .string()
+      .refine((val) => !isNaN(Date.parse(val)), {
+        message: "Date of birth is required",
+      }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
